9cefa0400f0fbd871ec3f27f2162a314e93af0ae
Mohsin Hassan <mohsin-vscode@mhlab.me>
2024-02-03 16:00:20 +0400

    added output to csv

--- scrape_carrefour.py
+++ scrape_carrefour.py
@@ -1,6 +1,10 @@
 from urllib.parse import urljoin, urlencode
 import requests
 import json
+from datetime import datetime
+import csv
+
+current_date = datetime.now().strftime('%Y-%m-%d')
 
 global_headers = {
     'Accept': '*/*',
@@ -18,6 +22,7 @@ def get_categories(main_url, category_name, headers):
     response_json = json.loads(response.text)
 
     categories_list = []
+    # total_product_count = []
 
     for element in response_json:
         for parent in element.get('children', []):
@@ -31,22 +36,20 @@ def get_categories(main_url, category_name, headers):
                             'category_id': Category_L3_id,
                             'product_count': product_count
                         }
+                        #total_product_count.append(product_count)
 
                         categories_list.append(category_dict)
-
+    # print(sum(total_product_count))
     return categories_list
 
 def construct_url(category_id, product_count):
     # Base URL
     base_url = "https://www.carrefouruae.com/api/v8/categories/"
 
-    # Relative path
-    #relative_path = category_id
-
     # Query parameters
     params = {
         'currentPage': 0,
-        'pageSize': 1, #product_count
+        'pageSize': 1, #product_count,
         'lang': 'en',
         'displayCurr': 'AED',
         'latitude': 25.2171003,
@@ -68,9 +71,20 @@ def process_product_data(url, headers):
     processed_data = []
 
     for product in clean_json:
-        product_id = product['id']
-        name = product['name']
-        price = product['price']['price']
+        product_id = product.get('id')
+        product_ean = product.get('ean')
+        
+        # Remove newline characters and white spaces from the 'name' variable
+        name = product.get('name', '').replace('\n', '').strip()
+
+        categories = product.get('category', [])
+        Categorie_L1 = categories[0].get('name') if categories and len(categories) > 0 else None
+        Categorie_L2 = categories[1].get('name') if categories and len(categories) > 1 else None
+        Categorie_L3 = categories[2].get('name') if categories and len(categories) > 2 else None
+
+        Brand = product.get('brand', {}).get('name')
+        price = product.get('price', {}).get('price')
+        seller = product.get('supplier')
 
         try:
             discount_price = product['price']['discount']['price']
@@ -79,6 +93,10 @@ def process_product_data(url, headers):
 
         product_info = {
             'product_id': product_id,
+            'product_ean': product_ean,
+            'Categorie_L1': Categorie_L1,
+            'Categorie_L2': Categorie_L2,
+            'Categorie_L3': Categorie_L3,
             'name': name,
             'price': price,
             'discount_price': discount_price
@@ -92,9 +110,27 @@ main_url = 'https://www.carrefouruae.com/api/v1/menu?latitude=25.2171003&longitu
 category_name = "Electronics & Appliances"
 
 result_categories = get_categories(main_url, category_name, global_headers)
+
+data_for_csv = []
+
 for category_info in result_categories:
     category_id = category_info['category_id']
     product_count = category_info['product_count']
     url = construct_url(category_id, product_count)
     data = process_product_data(url, global_headers)
-    print(data)
+    data_for_csv.extend(data)
+
+csv_file_path = f'carrefour_UAE_{current_date}.csv'
+
+# Writing data to the CSV file
+with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+    # Define the CSV writer
+    csv_writer = csv.DictWriter(csv_file, fieldnames=data[0].keys())
+
+    # Write the header
+    csv_writer.writeheader()
+
+    # Write the data
+    csv_writer.writerows(data_for_csv)
+
+print(f'Data has been exported to {csv_file_path}')
